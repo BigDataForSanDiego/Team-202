b79a73ab66a903f6840eb5b46693d4af502c26ce
Aaron Cayanan <aaroncayanan02@gmail.com>
2024-10-16 03:40:07 -0700

    Initial Commit + Change Background Image

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Background />
+    </div>
+  );
+}
+
+function Background() {
+  return (
+    <div className="background-image">
+
+    </div>
+  )
+}
+
+export default App;
